7c1ff1cf4c2084c658973b75421b2296fcef26d9
cedric hamilton <cedricjhamilton@gmail.com>
2023-11-09 18:13:56 -0500

    moved variables to correct place

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   var password = "";
-  var possible = "";
+  var possiblecharacters = "";
   var lowercase = 'abcdefghijklmnopqrstuvwxyz'
   var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   var specialcharacters = '!@#$%^&*_'
@@ -15,37 +15,41 @@ function generatePassword() {
   // var hasspecialcharacters = true;
   // var hasnumbers = true;
 
+  var length = window.prompt('how many characters do you want your password to be')
+  console.log(length)
+  if (length < 8 ||  length > 128){
+    alert("password not long enough") 
+    return null
+  } 
+  var lowercaseanswer = window.confirm('do you want to have lower case letters in it?')
 
-
-  if (haslowercaseanswer) {
+  if (lowercaseanswer) {
     possiblecharacters += lowercase;
     console.log(possiblecharacters);
   }
-
-  if (hasuppercaseanswer) {
+  var uppercaseanswer = window.confirm('do you want to have upper case letters in it?')
+  if (uppercaseanswer) {
     possiblecharacters += uppercase;
     console.log(possiblecharacters);
   }
-
-  if (hasspecialcharacters) {
+  var specialcharactersanswer = window.confirm('do you want to have special characters in it?')
+  if (specialcharactersanswer) {
     possiblecharacters += specialcharacters;
     console.log(possiblecharacters);
   }
+  var numbersanswer = window.confirm('do you want it to have numbers in it?')
 
-  if (hasnumbersanswer) {
+  if (numbersanswer) {
     possiblecharacters += numbers;
     console.log(possiblecharacters);
   }
 
 
-  var length = window.prompt('how many characters do you want your password to be')
-  console.log(length)
-  var lowercaseanswer = window.confirm('do you want to have lower case letters in it?')
-  var uppercaseanswer = window.confirm('do you want to have upper case letters in it?')
-  var specialcharactersanswer = window.confirm('do you want to have special characters in it?')
-  var numbersanswer = window.confirm('do you want it to have numbers in it?')
+  
+  
+  
 
-  for (var i = 0; i < inputLength; i++) {
+  for (var i = 0; i < length; i++) {
     var random = Math.floor(Math.random() * possiblecharacters.length);
     password += possiblecharacters[random];
   }
